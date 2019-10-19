63d4af7b22114f15b24892a9d59cb8f1e92abac0
Maciej Czuchnowski <mczuchnowski@users.noreply.github.com>
2019-10-19 14:03:20 +0200

    Accept alternative filename

--- lib/data_generator.rb
+++ lib/data_generator.rb
@@ -5,20 +5,22 @@ BANK_NAME_COL = 1
 BRANCH_IDENTIFIER_COL = 4
 BRANCH_NAME_COL = 5
 DATA_DIR_NAME = 'data'.freeze
-FILE_NAME = 'plewibnra.txt'.freeze
+FILENAMES = ['plewibnra.txt', 'PlainDok.txt'].freeze
+
+filename = FILENAMES.detect { |file| File.exist?(file) }
 
 FileUtils.mkdir_p(DATA_DIR_NAME) unless File.directory?(DATA_DIR_NAME)
 
-file = File.open(FILE_NAME, 'r', encoding: 'CP852')
+file = File.open(filename, 'r', encoding: 'CP852')
 
 file.each_line do |row|
-  row = row.encode(Encoding.find('UTF-8'), {invalid: :replace, undef: :replace, replace: ''}).split("\t").map(&:strip)
+  row = row.encode(Encoding.find('UTF-8'), invalid: :replace, undef: :replace, replace: '').split("\t").map(&:strip)
 
   branch_identifier = row[BRANCH_IDENTIFIER_COL]
   bank_identifier = branch_identifier.slice(0, 4)
   branch_name = row[BRANCH_NAME_COL]
 
-  file_name = "#{DATA_DIR_NAME}/#{bank_identifier}.yml"
+  data_file = "#{DATA_DIR_NAME}/#{bank_identifier}.yml"
 
   content = {
     branch_identifier.to_i => {
@@ -26,19 +28,19 @@ file.each_line do |row|
     }
   }
 
-  if File.exist?(file_name)
-    yaml_string = File.read(file_name)
+  if File.exist?(data_file)
+    yaml_string = File.read(data_file)
     data = YAML.safe_load(yaml_string)
 
     content = data.merge(content)
   else
-    File.new(file_name, 'w+')
+    File.new(data_file, 'w+')
 
     bank_name = row[BANK_NAME_COL].strip
     content = { 'name' => bank_name }.merge(content)
   end
 
-  File.open(file_name, 'r+') do |f|
+  File.open(data_file, 'r+') do |f|
     f.write(content.to_yaml(line_width: -1))
   end
 end
